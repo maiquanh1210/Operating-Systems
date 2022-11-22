1f0fd0c0461ca8f3409199cfaedba182d70e2bbc
Quynh Anh <89251525+maiquanh1210@users.noreply.github.com>
2022-11-22 11:50:08 +0700

    Update getraw.sh

--- monitor/getraw.sh
+++ monitor/getraw.sh
@@ -4,8 +4,8 @@ count=0
 while ((avail==1)) && ((count<=10))
 do
  raw=$(top -b -d 3 -n 1 | grep mutex | awk '{print $1 " " $6 " " $9}')
- if [[ $raw ]]; then
-  echo "$raw" >> data.txt
+ if [[ $ram ]]; then
+  echo "$ram" >> data.txt
   count=0
  else
   echo "done"
