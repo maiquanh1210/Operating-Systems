1ccea86e73cd47087a0e85444c63b5505fa412a4
Quynh Anh <89251525+maiquanh1210@users.noreply.github.com>
2022-11-22 11:50:47 +0700

    Update monitor.sh

--- monitor/monitor.sh
+++ monitor/monitor.sh
@@ -3,11 +3,8 @@ declare -a proc_time
 for i in {1..50}
 do
 	start=$(date +%s%4N);
-	./semaphore #| top -b -n 3 | grep mutex |tail -n 1 | awk '{print $1 " "$6}' 
-	#ram=$(./mutex ; pmap $(ps au | grep -m1 mutex) | tail -n 1 | awk '{print $2}')
+	./mutex
 	end=$(date +%s%4N)
-	#ram=$(sudo pmap "$PID" | tail -n 1 | awk '{print $2}')
-	#echo "$ram"
 	time="$(($end-$start))";
 	proc_time+=("$time")
 
